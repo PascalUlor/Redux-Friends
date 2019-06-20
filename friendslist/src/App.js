78d618ea352e113a332ea8feaae492dfcb03c981
PascalUlor <pascalulor@yahoo.com>
2019-06-20 16:05:44 +0100

    test login component

--- friendslist/src/App.js
+++ friendslist/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MainView from './views/MainView';
 
 
 function App() {
   return (
     <div className="App">
       I'm Great!!!!!!
+      <MainView />
     </div>
   );
 }
