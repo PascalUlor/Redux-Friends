184066d83d080a7c058fa3e06bf352fd16676775
PascalUlor <pascalulor@yahoo.com>
2019-06-21 05:47:01 +0100

    set loader for friendlist

--- friendslist/src/views/ProtectedRoute.jsx
+++ friendslist/src/views/ProtectedRoute.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect }from 'react';
 import { connect } from 'react-redux';
+import Loader from "react-loader-spinner";
 import { fetchData } from '../actions';
 import FriendPage from '../components/FrienPage';
 
 
 export function ProtectedRoute(props) {
   console.log('$$$$$$',props)
-  const { fetchData, friendList } = props;
-  console.log('+++++++++',friendList);
+  const { fetchData,friendList,fetching } = props;
+  console.log('+++++++++',fetching);
+  
   useEffect(()=>{
     fetchData()
   },[fetchData]);
+
+  if (fetching) {
+    return (<div>
+<Loader type="Circles" color="green" height="100" width="100" />
+    </div>);
+  }
+
   return (<div>
   <h1>Friend List</h1>
   {friendList.friends.map(fr=>{
     return <FriendPage friend={fr} key={fr.id}/>
   })}
   </div>)
+
 }
 
 const mapStateToProps =(state)=>{
   return {
-    friendList: state.friendList
+    friendList: state.friendList,
+    fetching: state.friendList.fetching
   };
 }
 
